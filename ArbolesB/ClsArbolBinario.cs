c4c7fe9e1db42d9fb1237d148c51a834bc9123c0
AllanRoca-Projects <82012129+AllanRoca-Projects@users.noreply.github.com>
2021-04-05 22:21:20 -0600

    Add files via upload

---
+++ ArbolesB/ClsArbolBinario.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesB
+{
+    class ClsArbolBinario
+    {
+        protected ClsNodo Raiz;
+
+        public ClsArbolBinario()
+        {
+            Raiz = null;
+        }
+
+        public bool ArbolVacio() 
+        {
+            if (Raiz == null) return true;
+            else return false;
+        }
+
+        public ClsNodo getRaiz() { return Raiz; }
+
+        public void AddDato(Object NodoDato, string cadena) 
+        {
+            ClsNodo Arb_Aux = null;
+            ClsNodo Arb_Padre = null;
+            ClsNodo NuevoNodo = new ClsNodo(NodoDato, cadena);
+
+            if (ArbolVacio())
+                Raiz = new ClsNodo(NodoDato, cadena) ;
+            else 
+            {
+                Arb_Aux = Raiz;
+                while (Arb_Aux != null)
+                {
+                    if(Arb_Aux.getValNodo() > NuevoNodo.getValNodo()) 
+                    {
+                        Arb_Padre = Arb_Aux;
+                        Arb_Aux = Arb_Aux.subArbolIzq();
+                    }else if (Arb_Aux.getValNodo() < NuevoNodo.getValNodo())
+                    {
+                        Arb_Padre = Arb_Aux;
+                        Arb_Aux = Arb_Aux.subArbolDer();
+                    }
+
+                }
+
+                if (Arb_Padre.getValNodo() > NuevoNodo.getValNodo())
+                {
+                    Arb_Padre.setRamaIzq(NuevoNodo);
+                }
+                if (Arb_Padre.getValNodo() < NuevoNodo.getValNodo())
+                {
+                     Arb_Padre.setRamaDer(NuevoNodo);
+                }
+            }
+        }//Fin AddDato
+
+        public object BuscarDato(string cadena) 
+        {
+            ClsNodo Arb_Aux = null;
+            ClsNodo Arb_Padre = null;
+            int ValCadena = ValorCadena(cadena);
+
+            if (ArbolVacio())
+                return null;
+            else
+            {
+                Arb_Aux = Raiz;
+                while (Arb_Aux.getValNodo() != ValCadena)
+                {
+                    if (Arb_Aux.getValNodo() > ValCadena)
+                    {
+                        Arb_Padre = Arb_Aux;
+                        Arb_Aux = Arb_Aux.subArbolIzq();
+                    }
+                    else if (Arb_Aux.getValNodo() < ValCadena)
+                    {
+                        Arb_Padre = Arb_Aux;
+                        Arb_Aux = Arb_Aux.subArbolDer();
+                    }
+
+                }
+                return Arb_Aux.getDato();
+
+            }
+        }
+
+        private int ValorCadena(string cadena)
+        {
+            char c = ' ';
+            int suma = 0;
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+
+                try
+                {
+                    c = cadena[i];
+                    Console.WriteLine(c);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("No se pudo" + e);
+                }
+                suma += Convert.ToInt32(c);
+            }
+
+            return suma;
+        }
+        public ClsCola inorden(ClsNodo Raiz, ClsCola Cola) 
+        {
+            ClsCola ColaN = new ClsCola();
+
+            if (Raiz != null) 
+            {
+                ColaN =  inorden(Raiz.subArbolIzq(), ColaN);
+                ColaN.push(Raiz.getDato());
+                ColaN = inorden(Raiz.subArbolIzq(), ColaN);
+            }
+            return ColaN;
+
+        }
+
+    }
+
+}
---
+++ ArbolesB/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ArbolesB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnadir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.txtProgramador = new System.Windows.Forms.TextBox();
+            this.txtDuracion = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lstDatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAnadir
+            // 
+            this.btnAnadir.Location = new System.Drawing.Point(436, 128);
+            this.btnAnadir.Name = "btnAnadir";
+            this.btnAnadir.Size = new System.Drawing.Size(135, 47);
+            this.btnAnadir.TabIndex = 0;
+            this.btnAnadir.Text = "Añadir ";
+            this.btnAnadir.UseVisualStyleBackColor = true;
+            this.btnAnadir.Click += new System.EventHandler(this.btnAnadir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre tarea";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Duracion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Porcentaje de avance";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Programador Asignado";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(202, 81);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(195, 39);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Location = new System.Drawing.Point(202, 223);
+            this.txtPorcentaje.Multiline = true;
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(195, 39);
+            this.txtPorcentaje.TabIndex = 6;
+            // 
+            // txtProgramador
+            // 
+            this.txtProgramador.Location = new System.Drawing.Point(202, 298);
+            this.txtProgramador.Multiline = true;
+            this.txtProgramador.Name = "txtProgramador";
+            this.txtProgramador.Size = new System.Drawing.Size(195, 41);
+            this.txtProgramador.TabIndex = 7;
+            // 
+            // txtDuracion
+            // 
+            this.txtDuracion.Location = new System.Drawing.Point(202, 145);
+            this.txtDuracion.Multiline = true;
+            this.txtDuracion.Name = "txtDuracion";
+            this.txtDuracion.Size = new System.Drawing.Size(195, 39);
+            this.txtDuracion.TabIndex = 8;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(436, 220);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(135, 48);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(175, 387);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(255, 45);
+            this.btnListar.TabIndex = 10;
+            this.btnListar.Text = "Listar Inorden";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // lstDatos
+            // 
+            this.lstDatos.FormattingEnabled = true;
+            this.lstDatos.ItemHeight = 16;
+            this.lstDatos.Location = new System.Drawing.Point(58, 465);
+            this.lstDatos.Name = "lstDatos";
+            this.lstDatos.Size = new System.Drawing.Size(513, 180);
+            this.lstDatos.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 687);
+            this.Controls.Add(this.lstDatos);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtDuracion);
+            this.Controls.Add(this.txtProgramador);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAnadir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnadir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+        private System.Windows.Forms.TextBox txtProgramador;
+        private System.Windows.Forms.TextBox txtDuracion;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.ListBox lstDatos;
+    }
+}
+
